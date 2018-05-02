7a10caff37e47af81620cc18350591aadfdbfaf0
boyd ndonga <boydbugle@gmail.com>
2018-05-02 19:41:57 +0300

    debug activate event listener

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+var dataCacheName = 'weatherData-v1';
 var cacheName = 'weatherPWA-step-6-1';
 var filesToCache = [
   '/',
@@ -18,7 +19,6 @@ var filesToCache = [
   '/images/thunderstorm.png',
   '/images/wind.png'
 ];
-var dataCacheName = 'weatherData-v1';
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
@@ -34,13 +34,23 @@ self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        if if (key !== cacheName && key !== dataCacheName) {
+        if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
     })
   );
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
   return self.clients.claim();
 });
 self.addEventListener('fetch', function(e) {
